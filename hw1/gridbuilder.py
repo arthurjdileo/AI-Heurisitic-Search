3116b0f85f9f3385478f049d6766c054ed30c46e
Connor Aleksandrowicz <connoraz141@gmail.com>
2020-10-18 17:56:36 -0400

    Added

--- hw1/gridbuilder.py
+++ hw1/gridbuilder.py
@@ -20,6 +20,33 @@ class world:
             for row in self.data:
                 f.write(row)
                 f.write('\n')
+    def createBlocked(self):
+        #assign 20% of total board to blocked
+        for cell in range(3840):
+            row = randint(0,119)
+            col = randint(0,159)
+            if(self.data[row,col] == 'a' or self.data[row,col] == 'b'):
+                cell-=1
+            else:
+                self.data[row,col] = '0'
+    def in_bounds(self, cell):
+        x = cell[0]
+        y = cell[1]
+        if(0<=x<=119) and (0<=y<=159):
+            return True
+        else:
+            return False
+    def connected_cells(self, cell):
+        #cell is a list [row, col]
+        x = cell[0]
+        y = cell[1]
+        connected_cells = set()
+        all_possible = [(x, y+1), (x, y-1), (x+1, y+1), (x+1, y), (x+1, y-1), (x-1, y), (x-1, y+1), (x-1, y-1)]
+        for (row, col) in all_possible:
+            if self.in_bounds((row,col)):
+                if self.data[row,col] != '0':
+                    connected_cells.add((row,col))
+        return connected_cells
     def generateTexture(self):
         #Start, Goal, and hard_travers can all be found in load as global variables
         #randonly assign hard to traverse cells to the 31x31 grid surrounding the centers in hard_travers
@@ -35,6 +62,14 @@ class world:
         self.data[start[0],start[1]] = 'S'
         #assigns the goal index
         self.data[goal[0],goal[1]] = 'G'
+
+    def wipeHighways(self):
+        for pair in highwaylist:
+            if self.data[pair[0], pair[1]] == 'a':
+                self.data[pair[0], pair[1]] = 1
+            elif self.data[pair[0], pair[1]] == 'b':
+                self.data[pair[0], pair[1]] = 2
+
     def createHighways(self):
         global highwaylist
         highwaylist = []
@@ -515,20 +550,72 @@ class world:
             if count < 100:
                 continue
 
+def randomize():
+    #Defines start and Goal
+    global start
+    start = [None, None]
+    global goal
+    goal = [None, None]
+
+    r1 = randint(0,3)
+    #North
+    if r1 == 0:
+        rows = randint(0,19)
+        cols = randint(0,159)
+        rowg = randint(99,119)
+        colg = randint(0,159)
+        start = [rows, cols]
+        goal = [rowg, colg]
+    #South
+    elif r1 == 1:
+        rows = randint(99,119)
+        cols = randint(0,159)
+        rowg = randint(0,19)
+        colg = randint(0,159)
+        start = [rows, cols]
+        goal = [rowg, colg]
+    #East
+    elif r1 == 2:
+        rows = randint(0,119)
+        cols = randint(139,159)
+        rowg = randint(0,119)
+        colg = randint(0,19)
+        start = [rows, cols]
+        goal = [rowg, colg]
+    #West
+    elif r1 == 3:
+        rows = randint(0,119)
+        cols = randint(0,19)
+        rowg = randint(0,119)
+        colg = randint(139,159)
+        start = [rows, cols]
+        goal = [rowg, colg]
+
+
+
+    global hard_travers
+    hard_travers = []
+    for _ in range(8):
+        hard_travers.append([randint(0,119), randint(0,159)])
+
 def load():
-    if(raw_input("Would you like to load a file? (y/n): ") != "y"):
+    if(input("Would you like to load a file? (y/n): ") != "y"):
         randomize()
         return
     with open(input("Please enter the path to input file: "), 'r') as f:
         input_list = f.read().strip().split("\n")
         #First line will provide the coordinates of start
         global start
-        start = input_list[0].split(",")
-        start = map(int, start)
+        start = [None, None]
+        token = input_list[0].split(",")
+        start[0] = int(token[0])
+        start[1] = int(token[1])
         #Second line will provide the coordinates of goal
         global goal
-        goal = input_list[1].split(",")
-        goal = map(int, goal)
+        goal = [None, None]
+        token = input_list[1].split(",")
+        goal[0] = int(token[0])
+        goal[1] = int(token[1])
         #Next EIGHT lines will provide the coordinates of the centers of the hard to traverse regions
         global hard_travers
         hard_travers = [input_list[2].split(","), input_list[3].split(","), input_list[4].split(","), input_list[5].split(","), input_list[6].split(","), input_list[7].split(","), input_list[8].split(","), input_list[9].split(",")]
@@ -550,19 +637,15 @@ def unload():
 
         f.write("Highway List: ")
         for pair in highwaylist:
-            f.write(str(pair[0])+","+str(pair[1])+"\n")
-
-def randomize():
-    global start
-    global goal
-    global hard_travers
+            f.write(str(pair[0])+","+str(pair[1])+ "\n")
 
 def main():
-	load()
-	w = world()
-	w.generateTexture()
-	w.createHighways()
-	w.printworld()
+    load()
+    w = world()
+    w.generateTexture()
+    w.createHighways()
+    w.createBlocked()
+    w.printworld()
 
 
 if __name__ == "__main__":
